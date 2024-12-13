146674f92f3fb0d30afde7914e9a2f1f56c69bff
hamza <hamzahelal17315@hotmail.com>
2024-12-13 21:38:03 +0200

    JS

---
+++ project1.js
@@ -0,0 +1,29 @@
+
+var num1=+prompt("enter first number")
+var num2=+prompt("enter sucand number")
+sum=num1+num2;
+document.write("<h2>The sum of plus:" + sum + "</h2>");
+
+
+// =====================
+
+var num3=-prompt("enter first number")
+var num4=-prompt("enter sucand number")
+sum=num3-num4;
+document.write("<h2>The sum of Subtract:" + sum + "</h2>");
+
+
+// ============================
+
+var num5=prompt("enter first number")
+var num6=prompt("enter sucand number")
+sum=num5*num6;
+document.write("<h2>The sum of multiplication:" + sum + "</h2>");
+
+// =============================
+
+var num7=prompt("enter first number")
+var num8=prompt("enter sucand number")
+sum=num7/num8;
+document.write("<h2>The sum of portion:" + sum + "</h2>");
+
